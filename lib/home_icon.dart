3fb9be376c0e09df9032ca0f574ec28d5b015650
frchick <frchick.00@gmail.com>
2022-10-19 09:53:26 +0900

    家アイコンのソースを別ファイルに

---
+++ lib/home_icon.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+import 'mydrag_target.dart';  // メンバー一覧のマーカー
+
+import 'text_ballon_widget.dart';
+import 'members.dart';
+import 'tatsumas.dart';
+import 'globals.dart';
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+// 家ボタン＆メンバー一覧メニュー
+class HomeIconWidget extends StatefulWidget
+{
+  HomeIconWidget({
+    super.key,
+  });
+
+  @override
+  State<HomeIconWidget> createState() => _HomeIconWidgetState();
+}
+
+class _HomeIconWidgetState extends State<HomeIconWidget>
+{
+  late StateSetter _setModalState;
+
+  // メンバーメニュー領域の高さ
+  static const double menuHeight = 120;
+
+  //----------------------------------------------------------------------------
+  // メンバー一覧メニューからドラッグして出動！
+  void onDragEndFunc(MyDraggableDetails details)
+  {
+    // メンバー一覧メニューの外にドラッグされていなければ何もしない。
+    // ドラッグ座標はマーカー左上なので、下矢印の位置にオフセットする。
+    var px = details.offset.dx + 32;
+    var py = details.offset.dy + 72;
+    final double screenHeight = getScreenHeight();
+    if((screenHeight - menuHeight) < py) return;
+  
+    // ドラッグ座標からマーカーの緯度経度を計算
+    LatLng? point = mainMapController.pointToLatLng(CustomPoint(px, py));
+    if(point == null) return;
+
+    // タツママーカーにスナップ
+    point = snapToTatsuma(point);
+
+    // メニュー領域の再描画
+    final int index = details.data;
+    _setModalState?.call((){
+      // データとマップ上マーカーを出動/表示状態に
+      members[index].attended = true;
+      memberMarkers[index].visible = true;
+      if(point != null){
+        members[index].pos = point;
+        memberMarkers[index].point = point;
+      }
+    });
+
+    // 地図上のマーカーの再描画
+    updateMapView();
+
+    // データベースに変更を通知
+    syncMemberState(index);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Align(
+      // 画面右下に配置
+      alignment: const Alignment(1.0, 1.0),
+      // 家アイコンとそのスタイル
+      child: ElevatedButton(
+        child: const Icon(Icons.home, size: 50),
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.orange.shade900,
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          fixedSize: Size(80,80),
+        ),
+
+        // 家ボタンタップでメンバー一覧メニューを開く
+        onPressed: ()
+        {
+          showMembersBottomSheet(context);
+        },
+
+        // 長押しでサブメニュー
+        onLongPress: (){
+          // 編集ロックならサブメニュー出さない
+          if(lockEditing) return;
+          showPopupMenu(context);
+        },
+      )
+    );
+  }
+
+  //----------------------------------------------------------------------------
+  // 画面下部のメンバー一覧ボトムシートを開く
+  void showMembersBottomSheet(BuildContext context)
+  {
+    // メンバー一覧メニューを開く
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context)
+      {
+        return StatefulBuilder(
+          builder: (context, StateSetter setModalState)
+          {
+            _setModalState = setModalState;
+
+            // 出動していないメンバーのアイコンを並べる
+            // NOTE: メンバーをドラッグで地図に配置した際、この StatefulBuilder.builder() で
+            // NOTE: 再描画を行う。そのためアイコンリストの構築はココに実装する必要がある。
+            // NOTE: 退会者はここに表示しないことで、新たにマップ上に配置できないようにする。
+            List<Widget> draggableIcons = [];
+            for(int index = 0; index < members.length; index++)
+            {
+              final Member member = members[index];
+              if(member.attended || member.withdrawals) continue;
+
+              final String name = members[index].name;
+              draggableIcons.add(Align(
+                alignment: const Alignment(0.0, -0.8),
+                child: GestureDetector(
+                  child: MyDraggable<int>(
+                    data: index,
+                    child: member.icon0,
+                    feedback: member.icon0,
+                    childWhenDragging: Container(
+                      width: 64,
+                      height: 72,
+                    ),
+                    onDragEnd: onDragEndFunc,
+                    // 編集がロックされいたらドラッグによる出動を抑止
+                    maxSimultaneousDrags: (lockEditing? 0: null),
+                  ),
+                  // タップして名前表示
+                  onTap: (){
+                    // ポップアップメッセージ
+                    showTextBallonMessage(name);
+                  }
+                )
+              ));
+            };
+
+            // 高さ120ドット、横スクロールのリストビュー
+            final ScrollController controller = ScrollController();
+            return Container(
+              height: menuHeight,
+              color: Colors.brown.shade100,
+              child: Scrollbar(
+                thumbVisibility: true,
+                controller: controller,
+                child: ListView(
+                  controller: controller,
+                  scrollDirection: Axis.horizontal,
+                  children: draggableIcons,
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  //----------------------------------------------------------------------------
+  // 家アイコン長押しのポップアップメニュー
+  void showPopupMenu(BuildContext context)
+  {
+    final double x = context.size!.width;
+    final double y = context.size!.height - 150;
+    
+    // Note: アイコンカラーは ListTile のデフォルトカラー合わせ
+    showMenu(
+      context: context,
+      position: RelativeRect.fromLTRB(x, y, 0, 0),
+      elevation: 8.0,
+      items: [
+        PopupMenuItem(
+          value: 0,
+          child: Row(
+            children: [
+              Icon(Icons.hotel, color: Colors.black45),
+              const SizedBox(width: 5),
+              const Text('全員家に帰る'),
+            ]
+          ),
+          height: (kMinInteractiveDimension * 0.8),
+        ),
+      ],
+    ).then((value) {
+      switch(value ?? -1){
+      case 0:
+        // 全員を家に帰す
+        if(goEveryoneHome()){
+          updateMapView();
+        }
+        break;
+      }
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,22 @@ import 'package:flutter_localizations/flutter_localizations.dart';  // カレン
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:tatsumao/onoff_icon_button.dart';
 import 'firebase_options.dart';
 import 'dart:async';
 import 'package:google_fonts/google_fonts.dart';  // フォント
 import 'mydragmarker.dart';   // マップ上のマーカー
-import 'mydrag_target.dart';  // メンバー一覧のマーカー
 
 import 'file_tree.dart';
 import 'text_ballon_widget.dart';
 import 'members.dart';
 import 'tatsumas.dart';
 import 'ok_cancel_dialog.dart';
+import 'onoff_icon_button.dart';
+import 'home_icon.dart';
 import 'globals.dart';
 
 //----------------------------------------------------------------------------
 // グローバル変数
-
-// アイコンボタン共通のスタイル
-final ButtonStyle _appIconButtonStyle = ElevatedButton.styleFrom(
-  foregroundColor: Colors.orange.shade900,
-  backgroundColor: Colors.transparent,
-  shadowColor: Colors.transparent,
-  fixedSize: Size(80,80),
-);
 
 // 処理中インジケータ
 enum ProgressIndicatorState {
@@ -47,9 +39,6 @@ ProgressIndicatorState _progressIndicatorState = ProgressIndicatorState.NoIndica
 //---------------------------------------------------------------------------
 // メンバーデータの同期(firebase realtime database)
 FirebaseDatabase database = FirebaseDatabase.instance;
-
-// 編集がロックされているか
-bool lockEditing = false;
 
 //----------------------------------------------------------------------------
 // メンバー達の位置へマップを移動する
@@ -69,200 +58,6 @@ void moveMapToLocationOfMembers()
     options: FitBoundsOptions(
       padding: EdgeInsets.all(64),
       maxZoom: 16));
-}
-
-//----------------------------------------------------------------------------
-//----------------------------------------------------------------------------
-// 家ボタン＆メンバー一覧メニュー
-class HomeButtonWidget extends StatefulWidget
-{
-  HomeButtonWidget({
-    super.key,
-  });
-
-  @override
-  State<HomeButtonWidget> createState() => _HomeButtonWidgetState();
-}
-
-class _HomeButtonWidgetState extends State<HomeButtonWidget>
-{
-  late StateSetter _setModalState;
-
-  // メンバーメニュー領域の高さ
-  static const double menuHeight = 120;
-
-  //----------------------------------------------------------------------------
-  // メンバー一覧メニューからドラッグして出動！
-  void onDragEndFunc(MyDraggableDetails details)
-  {
-    print("Draggable.onDragEnd: wasAccepted: ${details.wasAccepted}, velocity: ${details.velocity}, offset: ${details.offset}, data: ${details.data}");
-
-    // メンバー一覧メニューの外にドラッグされていなければ何もしない。
-    // ドラッグ座標はマーカー左上なので、下矢印の位置にオフセットする。
-    var px = details.offset.dx + 32;
-    var py = details.offset.dy + 72;
-    final double screenHeight = getScreenHeight();
-    if((screenHeight - menuHeight) < py) return;
-  
-    // ドラッグ座標からマーカーの緯度経度を計算
-    LatLng? point = mainMapController.pointToLatLng(CustomPoint(px, py));
-    if(point == null) return;
-
-    // タツママーカーにスナップ
-    point = snapToTatsuma(point);
-
-    // メニュー領域の再描画
-    final int index = details.data;
-    if(_setModalState != null){
-      _setModalState((){
-        // データとマップ上マーカーを出動/表示状態に
-        members[index].attended = true;
-        memberMarkers[index].visible = true;
-        if(point != null){
-          members[index].pos = point;
-          memberMarkers[index].point = point;
-        }
-      });
-    }
-
-    // 地図上のマーカーの再描画
-    updateMapView();
-
-    // データベースに変更を通知
-    syncMemberState(index);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context)
-  {
-    return Align(
-      // 画面右下に配置
-      alignment: const Alignment(1.0, 1.0),
-      // 家アイコンとそのスタイル
-      child: ElevatedButton(
-        child: const Icon(Icons.home, size: 50),
-        style: _appIconButtonStyle,
-
-        // 家ボタンタップでメンバー一覧メニューを開く
-        onPressed: ()
-        {
-          // メンバー一覧メニューを開く
-          showModalBottomSheet<void>(
-            context: context,
-            builder: (BuildContext context)
-            {
-              return StatefulBuilder(
-                builder: (context, StateSetter setModalState)
-                {
-                  _setModalState = setModalState;
-
-                  // 出動していないメンバーのアイコンを並べる
-                  // NOTE: メンバーをドラッグで地図に配置した際、この StatefulBuilder.builder() で
-                  // NOTE: 再描画を行う。そのためアイコンリストの構築はココに実装する必要がある。
-                  // NOTE: 退会者はここに表示しないことで、新たにマップ上に配置できないようにする。
-                  List<Widget> draggableIcons = [];
-                  int index = 0;
-                  members.forEach((member)
-                  {
-                    if(!member.attended && !member.withdrawals){
-                      final String name = members[index].name;
-                      draggableIcons.add(Align(
-                        alignment: const Alignment(0.0, -0.8),
-                        child: GestureDetector(
-                          child: MyDraggable<int>(
-                            data: index,
-                            child: member.icon0,
-                            feedback: member.icon0,
-                            childWhenDragging: Container(
-                              width: 64,
-                              height: 72,
-                            ),
-                            onDragEnd: onDragEndFunc,
-                            // 編集がロックされいたらドラッグによる出動を抑止
-                            maxSimultaneousDrags: (lockEditing? 0: null),
-                          ),
-                          // タップして名前表示
-                          onTap: (){
-                            // ポップアップメッセージ
-                            showTextBallonMessage(name);
-                          }
-                        )
-                      ));
-                    }
-                    index++;
-                  });
-                  // 高さ120ドット、横スクロールのリストビュー
-                  final ScrollController controller = ScrollController();
-                  return Container(
-                    height: menuHeight,
-                    color: Colors.brown.shade100,
-                    child: Scrollbar(
-                      thumbVisibility: true,
-                      controller: controller,
-                      child: ListView(
-                        controller: controller,
-                        scrollDirection: Axis.horizontal,
-                        children: draggableIcons,
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          );
-        },
-
-        // 長押しでサブメニュー
-        onLongPress: (){
-          // 編集ロックならサブメニュー出さない
-          if(lockEditing) return;
-          showPopupMenu(context);
-        },
-      )
-    );
-  }
-
-  //----------------------------------------------------------------------------
-  // 家アイコン長押しのポップアップメニュー
-  void showPopupMenu(BuildContext context)
-  {
-    final double x = context.size!.width;
-    final double y = context.size!.height - 150;
-    
-    // Note: アイコンカラーは ListTile のデフォルトカラー合わせ
-    showMenu(
-      context: context,
-      position: RelativeRect.fromLTRB(x, y, 0, 0),
-      elevation: 8.0,
-      items: [
-        PopupMenuItem(
-          value: 0,
-          child: Row(
-            children: [
-              Icon(Icons.hotel, color: Colors.black45),
-              const SizedBox(width: 5),
-              const Text('全員家に帰る'),
-            ]
-          ),
-          height: (kMinInteractiveDimension * 0.8),
-        ),
-      ],
-    ).then((value) {
-      switch(value ?? -1){
-      case 0:
-        // 全員を家に帰す
-        if(goEveryoneHome()){
-          updateMapView();
-        }
-        break;
-      }
-    });
-  }
 }
 
 //----------------------------------------------------------------------------
@@ -605,12 +400,11 @@ class _MapViewState extends State<MapView> with AfterLayoutMixin<MapView>
                     var tatsuma = tatsumas[index];
                     showChangeTatsumaDialog(context, tatsuma).then((res){
                       if(res != null){
-                        setState((){
-                          tatsuma.name     = res["name"] as String;
-                          tatsuma.visible  = res["visible"] as bool;
-                          tatsuma.areaBits = res["areaBits"] as int;
-                          updateTatsumaMarkers();
-                        });
+                        // タツマデータに反映
+                        tatsuma.name     = res["name"] as String;
+                        tatsuma.visible  = res["visible"] as bool;
+                        tatsuma.areaBits = res["areaBits"] as int;
+                        updateTatsumaMarkers();
                         // データベースに同期
                         updateTatsumaToDB(index);
                       }
@@ -637,7 +431,7 @@ class _MapViewState extends State<MapView> with AfterLayoutMixin<MapView>
             ),
 
             // 家アイコン
-            HomeButtonWidget(),
+            HomeIconWidget(),
 
             // ポップアップメッセージ
             Align(
